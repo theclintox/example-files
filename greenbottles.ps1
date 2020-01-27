397fb2289de0a98b3b521da088a253fa2aefe662
theclintox <52027991+theclintox@users.noreply.github.com>
2020-01-27 14:14:31 +0000

    Create greenbottles.ps1

---
+++ greenbottles.ps1
@@ -0,0 +1,6 @@
+# Ten Green Bottles
+for ($i = 10; $i -gt 0; $i --) {
+  Write-Host -ForegroundColor "Green" -Object "$i green bottles hanging on the wall"
+}
+
+Write-Host -ForegroundColor "Magenta" -Object "No green bottles hanging on the wall :-("
